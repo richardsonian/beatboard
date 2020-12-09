cb3bb2273a7ade7e3710a17cddcda82152af9b1d
Ian Richardson <ian.ross.richardson@gmail.com>
2020-12-09 18:57:08 -0500

    Add mini matrix test

---
+++ test/pad4pi_mini_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from pad4pi import rpi_gpio
+import time
+
+num_rows = 2
+num_cols = 2
+
+KEYPAD = [[(r, c) for c in range(1, num_cols + 1)] for r in range(1, num_rows + 1)]
+
+ROW_PINS = [13, 6] # BCM numbering
+COL_PINS = [26, 19] # BCM numbering
+
+factory = rpi_gpio.KeypadFactory()
+
+keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PINS)
+
+def printKey(key):
+    print(key)
+
+# printKey will be called each time a keypad button is pressed
+keypad.registerKeyPressHandler(printKey)
+
+print("Press a key (type exit to stop)")
+
+while True:
+    code = input()
+    if code == "exit":
+        keypad.cleanup() # Cleanup GPIO
+        break

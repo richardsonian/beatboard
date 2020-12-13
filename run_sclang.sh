aea33d5bbac5dd8d0bbdaf1d96c969d2622072cb
Ian Richardson <ian.ross.richardson@gmail.com>
2020-12-13 15:26:36 -0500

    add debug msg

--- run_sclang.sh
+++ run_sclang.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Run without arg for IDE mode, or pass file as argument (arg must be absolute path)
 FILE=$(readlink -f $1)
-echo $FILE
 pkill aplay
 export QT_QPA_PLATFORM=offscreen
 cd ~/supercolliderStandaloneRPI1
+echo $("Executing sc3 script: "$FILE)
 ./sclang -a -l ~/supercolliderStandaloneRPI1/sclang.yaml $FILE

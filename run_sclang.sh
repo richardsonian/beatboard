2743d51828d0c819ef433dee47ac008f7a30281c
Ian Richardson <ian.ross.richardson@gmail.com>
2020-12-13 17:33:20 -0500

    Remove sudo from run_sclang.sh

--- run_sclang.sh
+++ run_sclang.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # Run without arg for IDE mode, or pass file as argument (arg must be absolute path)
-if [ "$EUID" -ne 0 ]
-  then echo "Script must be run as root."
-  exit
-fi
-
+# Run "sudo pkill aplay before running this script"
 FILE=$(readlink -f $1)
-pkill aplay
 export QT_QPA_PLATFORM=offscreen
 cd /home/pi/supercolliderStandaloneRPI1
 echo "Executing sc3 script: ${FILE}"

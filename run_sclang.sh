eaed488f8ac3221b40aacee2cdde83487d036d93
Ian Richardson <ian.ross.richardson@gmail.com>
2020-12-13 15:17:21 -0500

    tweak

--- run_sclang.sh
+++ run_sclang.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Run without arg for IDE mode, or pass file as argument
+# Run without arg for IDE mode, or pass file as argument (arg must be absolute path)
+FILE=readlink -f $1
+echo $FILE
 pkill aplay
 export QT_QPA_PLATFORM=offscreen
 cd ~/supercolliderStandaloneRPI1
-./sclang -a -l ~/supercolliderStandaloneRPI1/sclang.yaml $1
+./sclang -a -l ~/supercolliderStandaloneRPI1/sclang.yaml $FILE

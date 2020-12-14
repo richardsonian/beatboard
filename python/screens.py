0c65c6b8691015940af3b1eb12213d33240b489e
Ian Richardson <ian.ross.richardson@gmail.com>
2020-12-13 19:07:45 -0500

    Joy click bugfix

--- python/screens.py
+++ python/screens.py
@@ -10,5 +10,5 @@ class Menu:
         print("menu left")
     def right(self):
         print("menu right")
-    def select(self):
+    def select(self, pin): #adding pin here as a hack to get GPIO.add_event_detect call not to throw error (passes pin as arg)
         print("menu select")

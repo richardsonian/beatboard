bd67b78ea307d6d85619844b56feaa3e09437b58
richardsonian <ian.irrtech@gmail.com>
2020-12-17 14:59:36 -0500

    Update run_sclang.sh

--- run_sclang.sh
+++ run_sclang.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-# Run without arg for IDE mode, or pass file as argument (arg must be absolute path)
-# Run "sudo pkill aplay before running this script"
-FILE=$(readlink -f $1)
+
+# SuperCollider Startup Script
+# Run without arg for IDE mode, or pass file as argument (arg may be relative path)
+# Run "sudo pkill aplay" before running this script
+
+# CONFIG:
+# The directory containing sclang
+SC3_PATH="/home/pi/supercolliderStandaloneRPI1"
+# Path to config file sclang.yaml
+SC3_CONFIG_PATH="~/supercolliderStandaloneRPI1/sclang.yaml"
+# Port for sclang to start on
+PORT=57120
+
+# SCRIPT
+CODE_PATH=$(readlink -f $1)
 export QT_QPA_PLATFORM=offscreen
-cd /home/pi/supercolliderStandaloneRPI1
-echo "Executing sc3 script: ${FILE}"
-./sclang -u 57120 -a -l ~/supercolliderStandaloneRPI1/sclang.yaml $FILE
+cd $SC3_PATH
+echo "Executing SC3 script: ${CODE_PATH}"
+./sclang -u $PORT -a -l $SC3_CONFIG_PATH $CODE_PATH
